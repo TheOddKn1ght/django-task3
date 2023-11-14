be9cebece46df00b55247a4556d7210413743309
oddknight <oddknight404@gmail.com>
2023-11-14 22:35:25 +0300

    add models in models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Answer(models.Model):
+    content = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
